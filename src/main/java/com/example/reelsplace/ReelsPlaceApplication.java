069518a3e4cc4e424d963a4b94111c1be8e17d10
grbuguj <bugs10613@gmail.com>
2025-12-16 22:12:04 +0900

    Initial commit

---
+++ src/main/java/com/example/reelsplace/ReelsPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reelsplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelsPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelsPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reelsplace/ReelsPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reelsplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReelsPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
